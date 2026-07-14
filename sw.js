98005edb8b8b99cc59a8f78ae98619c57744dba6
kurtyoung01-max <kurtyoung01@gmail.com>
2026-07-15 08:41:20 +1000

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'girlfriend-prize-wheel-v1';
-const FILES = ['./', './index.html', './style.css', './script.js', './manifest.json', './icon.svg'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request))));
