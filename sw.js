5afeb0bb109f0412c79a825c3261597fc946d90a
kurtyoung01-max <kurtyoung01@gmail.com>
2026-07-15 10:04:16 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,31 @@
-const CACHE = 'girlfriend-prize-wheel-v6-prize-editor';
+const CACHE = 'girlfriend-prize-wheel-v7-mobile-full-size';
 const FILES = ['./', './index.html', './style.css', './script.js', './manifest.json', './icon.svg'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request))));
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).catch(() => caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
